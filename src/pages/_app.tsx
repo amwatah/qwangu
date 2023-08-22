1028df9d73a8a9a336a5fe2909c7b29537df35f7
Amwatah Obuya <obuyaamwatah@gmail.com>
2023-08-22 08:29:21 +0300

    configured Mantine component library

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/app";
 import { api } from "@/utils/api";
 import "@/styles/globals.css";
+import AppLayout from "@/components/Layouts/AppLayout";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
 };
 
 export default api.withTRPC(MyApp);
